8367168e0730a4e8054a728b177ded50e8057077
Soham Roy <36095087+SohamRoyNoel@users.noreply.github.com>
2020-05-09 04:01:34 +0530

    Update Resourcepath.java

--- src/Paths/Resourcepath.java
+++ src/Paths/Resourcepath.java
@@ -2,12 +2,12 @@ package Paths;
 
 public class Resourcepath {
 
-	public static String sourcepath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Source";
-	public static String targetpath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Target";
-	public static String operationPath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Operation";
-	public static String execution = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Execution";
-	public static String excelReportColumnsPath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Operation\\ColumnList.properties";
-	public static String excelReportPath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\Operation\\Overall_test_report.xlsx";
-	public static String extentReportGenerationPath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham\\";
-	public static String noFileFoundPath = "\\\\ENTSSERVER85\\Cognizant\\Automation\\AutomationGeek\\Soham";
+	public static String sourcepath = "\\Source";
+	public static String targetpath = "\\Target";
+	public static String operationPath = "\\Operation";
+	public static String execution = "Execution";
+	public static String excelReportColumnsPath = "ColumnList.properties";
+	public static String excelReportPath = "\\Overall_test_report.xlsx";
+	public static String extentReportGenerationPath = "\\Soham\\";
+	public static String noFileFoundPath = "\\Soham";
 }
